31d3b54dfc1cd08d7535f7fbea7c5b2e5d00b7c0
Mina Nagy Zaki <mnzaki@gmail.com>
2022-12-05 19:10:58 +0100

    feat: implement strapi customFields API

--- server/index.js
+++ server/index.js
@@ -4,8 +4,18 @@ const services = require("./services");
 const routes = require("./routes");
 const controllers = require("./controllers");
 
+const pkgJson = require('../package.json');
+
 module.exports = {
   controllers,
   routes,
-  services
+  services,
+
+  register({ strapi }) {
+    strapi.customFields.register({
+      name: 'oembed',
+      plugin: pkgJson.strapi.name,
+      type: 'text'
+    })
+  }
 };
