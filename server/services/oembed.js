caefc666d666c5ef8029295290fdd30cca1cc53d
polem <paulemile.miny@gmail.com>
2023-07-31 12:43:38 +0200

    refactor(provider): first step of provider refactoring to improve code quality

--- server/services/oembed.js
+++ server/services/oembed.js
@@ -1,5 +1,7 @@
 'use strict';
+
 const axios = require('axios');
+const providers = require('./providers');
 
 /**
  * media-embed.js service
@@ -7,93 +9,44 @@ const axios = require('axios');
  * @description: A set of functions similar to controller's actions to avoid code duplication.
  */
 
-module.exports = (
-  {
-    strapi
-  }
-) => {
-  return {
-    async fetch(url) {
-      let data;
-
-      const matches = url.match(/^(https?:\/\/)?(www\.)?(youtu\.be|youtube\.com|soundcloud\.com|vimeo\.com|tiktok\.com|open\.spotify\.com)/i);
-
-      if (matches) {
-        try {
-          let fetchedData;
-          let title;
-          let mime;
-          let thumbnail;
-
-          switch (matches[3]) {
-            case 'youtu.be':
-            case 'youtube.com':
-              fetchedData = await axios.get(`https://www.youtube.com/oembed?url=${encodeURIComponent(url)}&format=json`).then(res => res.data);
-              title = fetchedData.title;
-              mime = 'video/youtube';
-              thumbnail = fetchedData.thumbnail_url;
-              break;
-            
-            case 'soundcloud.com':
-              fetchedData = await axios.get(`https://www.soundcloud.com/oembed?url=${encodeURIComponent(url)}&format=json`).then(res => res.data);
-              title = fetchedData.title;
-              mime = 'audio/soundcloud';
-              thumbnail = fetchedData.thumbnail_url;
-              break;
-            
-            case 'vimeo.com':
-              fetchedData = await axios.get(`https://vimeo.com/api/oembed.json?url=${encodeURIComponent(url)}`).then(res => res.data);
-              title = fetchedData.title;
-              mime = 'video/vimeo';
-              thumbnail = fetchedData.thumbnail_url;
-              break;
-
-            case 'tiktok.com':
-              fetchedData = await axios.get(`https://www.tiktok.com/oembed?url=${encodeURIComponent(url)}&format=json`).then(res => res.data);
-              title = fetchedData.title;
-              mime = 'video/tiktok';
-              thumbnail = fetchedData.thumbnail_url;
-              break;
-
-            case 'open.spotify.com':
-              fetchedData = await axios.get(`https://open.spotify.com/oembed?url=${encodeURIComponent(url)}`).then(res => res.data);
-              title = fetchedData.title;
-              mime = 'audio/spotify';
-              thumbnail = fetchedData.thumbnail_url;
-              break;
-          
-            default:
-              break;
-          }
-            
-          data = {
-            url,
-            title,
-            thumbnail,
-            mime,
-            rawData: fetchedData,
-          }
-          
-        } catch (error) {
-          if (error.response.status === 404) {
-            data = {
-              error: 'This URL can\'t be found'
-            }
-          } else if (error.response.status === 401) {
-            data = {
-              error: 'Embedding has been disabled for this media'
-            }
-          } else {
-            throw new Error(error);
-          }
-        }
-      } else {
-        data = {
-          error: 'Invalid URL'
+module.exports = ({ strapi }) => ({
+  async fetch(url) {
+    const matchedProvider = resolveProvider(url);
+  
+    if (!matchedProvider) {
+      return {
+        error: 'Invalid URL'
+      };
+    }
+  
+    try {
+      const fetchedData = await matchedProvider.embedData(url);
+  
+      return {
+        url,
+        title: fetchedData.title,
+        thumbnail: fetchedData.thumbnail_url,
+        mime: matchedProvider.mime,
+        rawData: fetchedData,
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 404) {
+          return {
+            error: 'This URL can\'t be found'
+          };
+        } else if (error.response?.status === 401) {
+          return {
+            error: 'Embedding has been disabled for this media'
+          };
         }
       }
-
-      return data;
+  
+      throw new Error(error);
     }
-  };
-};
+  },
+
+  resolveProvider(url) {
+    return Object.values(providers).find(provider => provider.pattern.test(url));
+  },
+})
